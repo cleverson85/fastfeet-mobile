c3d817c8c069e3c3de2b5805fd95bae8d86c4135
Cleverson Queiroz <cleverson.queiroz@ewave.com.br>
2020-07-02 00:22:12 -0400

    Adicionado camera

--- src/components/Action/Confirm/index.js
+++ src/components/Action/Confirm/index.js
@@ -1,20 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Alert } from 'react-native';
 
+import Camera from '../../Camera/index';
 import HeaderDetail from '~/components/HeaderDetail';
 import Button from '../../Button/index';
 import { Container } from '../../InfoDetail/styles';
 import { Background } from '../styles';
 
-function Confirm() {
+function Confirm({ route, navigation }) {
+  const handleSubmit = () => {
+    fetch('http://192.168.0.6:5555/deliveryEnd', {
+      method: 'POST',
+      body: JSON.stringify({}),
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+    })
+      .then((res) => res.json())
+      .catch((error) =>
+        Alert.alert('Aviso', 'Não foi possível enviar devido a um erro.')
+      )
+      .then((res) =>
+        Alert.alert('Aviso', 'Confirmação de entraga confirmada com sucesso.')
+      )
+      .then(() => navigation.goBack());
+  };
+
   return (
     <Background>
       <HeaderDetail title="Confimar entrega" />
       <View style={{ alignItems: 'center' }}>
         <Container>
+          <Camera />
           <Button
-            style={{ backgroundColor: '#7D40E7', width: '100%' }}
-            onPress={() => {}}
+            style={{ backgroundColor: '#7D40E7', width: '100%', marginTop: 10 }}
+            onPress={() => {
+              handleSubmit();
+            }}
           >
             Enviar
           </Button>
